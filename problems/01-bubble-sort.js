397e1006930b20ce04e4d92d36182a259b16aec7
username <mantaoxu2@gmail.com>
2021-05-18 09:50:13 -0700

    on the first problem

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -5,10 +5,25 @@ function swap(array, idx1, idx2) {
   array[idx1] = array[idx2];
   array[idx2] = temp;
 }
+let arr = [2, 8, 1, 4, 5]
+
 
 function bubbleSort(array) {
   // your code here
+  let swapped = true;
+
+  while (swapped){
+    swapped = false;
+    for(let i = 0; i < array.length; i++){
+      if(array[i] > array[i + 1]){
+        console.log(array);
+        swap(array, i, i + 1);
+        swapped = true;
+      }
+    }
+  }
 }
+bubbleSort(arr);
 
 module.exports = {
   bubbleSort,
