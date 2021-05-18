4b782970b106334edc64e15028969ae26551c6b1
username <mantaoxu2@gmail.com>
2021-05-18 10:48:45 -0700

    finished problem1-3

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,7 +1,26 @@
 // Implement Insertion Sort
 
+// let arr = [9, 2, 8, 5, 6]
+// [2, 9, 8, 5, 6]
+// [2, 8, 9, 5, 6]
+// [2, 5, 8, 9, 6]
+// [2, 5, 6, 8, 9]
+// arr.splice(1, 1, 4)
+// console.log(arr);
+
+
 function insertionSort(list) {
   // your code here
+  for(let i = 1; i < list.length; i++){
+    let temp = list[i];
+    for(let j = 0; j < list.length; j++){
+      if(temp < list[j]){
+          list.splice(i, 1);
+          list.splice(j, 0, temp);
+          break;
+        }
+      }
+  }
 }
 
 module.exports = {
