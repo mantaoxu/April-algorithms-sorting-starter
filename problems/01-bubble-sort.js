03e212a72800936e276524495159426ff83cb724
Brandon <omstachu626@gmail.com>
2021-05-18 10:11:58 -0700

    finished selection

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -1,31 +1,35 @@
 // Implement Bubble Sort
 
 function swap(array, idx1, idx2) {
-  let temp = array[idx1];
-  array[idx1] = array[idx2];
-  array[idx2] = temp;
+    let temp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = temp;
 }
 let arr = [2, 8, 1, 4, 5]
 
 
 function bubbleSort(array) {
-  // your code here
-  let swapped = true;
+    // your code here
+    let swapped = true;
+    let count = 0;
 
-  while (swapped){
-    swapped = false;
-    for(let i = 0; i < array.length; i++){
-      if(array[i] > array[i + 1]){
-        console.log(array);
-        swap(array, i, i + 1);
-        swapped = true;
-      }
+    while (swapped) {
+        count++;
+        // console.log(count);
+        swapped = false;
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array, i, i + 1);
+                // console.log(array);
+                swapped = true;
+            }
+        }
     }
-  }
+    return array;
 }
 bubbleSort(arr);
 
 module.exports = {
-  bubbleSort,
-  swap
+    bubbleSort,
+    swap
 };
--- problems/02-selection-sort.js
+++ problems/02-selection-sort.js
@@ -2,14 +2,28 @@
 
 // Implement swap without looking at bubble sort
 function swap(arr, index1, index2) {
-  // your code here
+    // your code here
+    let temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+
 }
 
 function selectionSort(list) {
-  // your code here
+    // your code here
+    for (let i = 0; i < list.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < list.length; j++) {
+            if (list[minIndex] > list[j]) {
+                minIndex = j;
+            }
+        }
+        swap(list, i, minIndex);
+    }
+    return list;
 }
 
 module.exports = {
-  selectionSort,
-  swap
+    selectionSort,
+    swap
 };
