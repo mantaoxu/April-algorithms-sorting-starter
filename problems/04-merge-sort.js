4d48f95b7f4a209f70284db73503f5c589eab865
username <mantaoxu2@gmail.com>
2021-05-18 15:43:00 -0700

    done merge function

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -1,35 +1,39 @@
 // Implement Merge Sort
-
-let arr1 = [3, 6, 8, 11];
-let arr2 = [1, 4, 7, 9]
+// [1, 5, 10, 15], [0, 2, 3, 7, 10]
+// let arr1 = [2, 3, 5, 9];
+// let arr2 = [1, 4, 7, 8]
     // [9, 4][10, 7]
     // [9][4] || [10][7]
     // [4, 9] || [7, 10]
     // [4, 7, 9, 10]
-
-
-
 
 function merge(array1, array2) {
     // your code here
     let newArr = [];
 
     while (array1.length || array2.length) {
-        console.log(array1[0], array2[0])
-        if (array1[0] < array2[0]) {
-            newArr.push(array1.shift());
+ 
+        if(!array1.length){
             newArr.push(array2.shift());
-        } else {
-            newArr.push(array2.shift());
-            newArr.push(array1.shift());
-        }
 
+        } else if(!array2.length){
+
+            newArr.push(array1.shift());
+
+        }
+        // console.log(array1[0], array2[0])
+        if(array1.length && array2.length){
+            if (array1[0] < array2[0]) {
+                newArr.push(array1.shift()); 
+            } else {
+                newArr.push(array2.shift());
+            }
+        }
     }
-    // console.log(newArr);
     return newArr;
 }
 
-console.log(merge(arr1, arr2));
+console.log(merge([1, 5, 10, 15], [0, 2, 3, 7, 10]));
 
 
 
